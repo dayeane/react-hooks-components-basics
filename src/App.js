8661cd2054012551ab659208c7c18da9527cdbda
Dayeane <dayeane85@gmail.com>
2021-10-30 20:23:30 -0700

    react comp basics

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ function App() {
   return (
     <div>
       <Article />
+      {/* <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div> */}
       <Comment />
+      {/* <div>Naturally, I agree with this article.</div>; */}
     </div>
   );
-}
+} 
+
+
+
 
 export default App;
